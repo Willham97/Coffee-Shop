6237ca153723faef6aad1f9a279a588ca4efbc3a
Willham97 <30661047+Willham97@users.noreply.github.com>
2018-10-24 15:56:08 -0500

    Add files via upload

--- CoffeeShop/CoffeeShop/AddOrdersForm.vb
+++ CoffeeShop/CoffeeShop/AddOrdersForm.vb
@@ -1,13 +1,10 @@
 ﻿Public Class frmAddOrders
-    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+    Private Sub btnSave_Click(sender As Object, e As EventArgs)
 
         Dim iProductID As String
         Dim iProductName As String
         Dim iCaseCost As String
 
-        iProductID = txtProductId.Text
-        iProductName = txtProductId.Text
-        iCaseCost = TextBox1.Text
 
         MessageBox.Show("Product ID: " & iProductID & vbNewLine & "Product Name:" & iProductName & vbNewLine & "Case Cost: " & iCaseCost)
 
@@ -15,5 +12,18 @@
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Close()
+    End Sub
+
+    Private Sub ProductTableBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles ProductTableBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.ProductTableBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.ProjectDatabaseDataSet)
+
+    End Sub
+
+    Private Sub frmAddOrders_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'ProjectDatabaseDataSet.ProductTable' table. You can move, or remove it, as needed.
+        Me.ProductTableTableAdapter.Fill(Me.ProjectDatabaseDataSet.ProductTable)
+
     End Sub
 End Class
