49af8b3c61e4c4f3327ac119c9157eb003747f6e
superstorm192 <dragon041801@gmail.com>
2018-10-31 21:11:45 -0500

    Schedule Form Update
    Note: I haven't renamed the actual variable from Schedual to Schedule yet to avoid possible compatibility issues.
    This is an update to the Schedule Form that has the ability to save/load from a separate .dat file that will be automatically created if it doesn't already exist. I also added print functionality to this form that I had gotten online, though I haven't tested if it actually would print yet.

--- CoffeeShop/CoffeeShop/SchedualForm.designer.vb
+++ CoffeeShop/CoffeeShop/SchedualForm.designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class frmSchedual
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SchedualForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()>
+    <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,162 +20,206 @@ Partial Class frmSchedual
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()>
+    <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
-        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
-        Me.ScheduleGrid = New System.Windows.Forms.DataGridView()
-        Me.btnPrint = New System.Windows.Forms.Button()
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnBack = New System.Windows.Forms.Button()
         Me.btnEmployees = New System.Windows.Forms.Button()
-        Me.btnMenu = New System.Windows.Forms.Button()
-        Me.ProjectDatabaseDataSet = New CoffeeShop.ProjectDatabaseDataSet()
-        Me.ScheduleTableBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.ScheduleTableTableAdapter = New CoffeeShop.ProjectDatabaseDataSetTableAdapters.ScheduleTableTableAdapter()
-        Me.TimeColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.MondayColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.TuesdayColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.WednesdayColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.ThursdayColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        CType(Me.ScheduleGrid, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.ProjectDatabaseDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.ScheduleTableBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.MonColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.MonColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.MonColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TueColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TueColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TueColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.WedColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.WedColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.WedColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ThurColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ThurColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ThurColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'ScheduleGrid
+        'DataGridView1
         '
-        Me.ScheduleGrid.AllowUserToAddRows = False
-        Me.ScheduleGrid.AllowUserToDeleteRows = False
-        Me.ScheduleGrid.AllowUserToResizeColumns = False
-        Me.ScheduleGrid.AllowUserToResizeRows = False
-        Me.ScheduleGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.ScheduleGrid.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.TimeColumn, Me.MondayColumn, Me.TuesdayColumn, Me.WednesdayColumn, Me.ThursdayColumn})
-        Me.ScheduleGrid.Location = New System.Drawing.Point(12, 12)
-        Me.ScheduleGrid.MultiSelect = False
-        Me.ScheduleGrid.Name = "ScheduleGrid"
-        Me.ScheduleGrid.ReadOnly = True
-        Me.ScheduleGrid.RowHeadersWidth = 25
-        Me.ScheduleGrid.RowTemplate.ReadOnly = True
-        Me.ScheduleGrid.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.ScheduleGrid.Size = New System.Drawing.Size(787, 595)
-        Me.ScheduleGrid.TabIndex = 0
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        DataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control
+        Me.DataGridView1.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle3
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        DataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle4
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.MonColumn, Me.MonColumn2, Me.MonColumn3, Me.TueColumn, Me.TueColumn2, Me.TueColumn3, Me.WedColumn, Me.WedColumn2, Me.WedColumn3, Me.ThurColumn, Me.ThurColumn2, Me.ThurColumn3})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.MultiSelect = False
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 90
+        Me.DataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(1293, 595)
+        Me.DataGridView1.TabIndex = 0
         '
-        'btnPrint
+        'btnBack
         '
-        Me.btnPrint.BackColor = System.Drawing.Color.Gold
-        Me.btnPrint.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnPrint.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.btnPrint.Location = New System.Drawing.Point(141, 623)
-        Me.btnPrint.Name = "btnPrint"
-        Me.btnPrint.Size = New System.Drawing.Size(157, 46)
-        Me.btnPrint.TabIndex = 75
-        Me.btnPrint.Text = "Print"
-        Me.btnPrint.UseVisualStyleBackColor = False
+        Me.btnBack.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnBack.BackColor = System.Drawing.Color.Gold
+        Me.btnBack.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBack.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnBack.Location = New System.Drawing.Point(245, 613)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(211, 55)
+        Me.btnBack.TabIndex = 4
+        Me.btnBack.Text = "Back"
+        Me.btnBack.UseVisualStyleBackColor = False
         '
         'btnEmployees
         '
+        Me.btnEmployees.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.btnEmployees.BackColor = System.Drawing.Color.Gold
-        Me.btnEmployees.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEmployees.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnEmployees.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.btnEmployees.Location = New System.Drawing.Point(333, 623)
+        Me.btnEmployees.Location = New System.Drawing.Point(455, 613)
         Me.btnEmployees.Name = "btnEmployees"
-        Me.btnEmployees.Size = New System.Drawing.Size(157, 46)
-        Me.btnEmployees.TabIndex = 74
+        Me.btnEmployees.Size = New System.Drawing.Size(211, 55)
+        Me.btnEmployees.TabIndex = 4
         Me.btnEmployees.Text = "Employees"
         Me.btnEmployees.UseVisualStyleBackColor = False
         '
-        'btnMenu
+        'btnPrint
         '
-        Me.btnMenu.BackColor = System.Drawing.Color.Gold
-        Me.btnMenu.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnMenu.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.btnMenu.Location = New System.Drawing.Point(527, 623)
-        Me.btnMenu.Name = "btnMenu"
-        Me.btnMenu.Size = New System.Drawing.Size(157, 46)
-        Me.btnMenu.TabIndex = 73
-        Me.btnMenu.Text = "Menu"
-        Me.btnMenu.UseVisualStyleBackColor = False
+        Me.btnPrint.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnPrint.BackColor = System.Drawing.Color.Gold
+        Me.btnPrint.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPrint.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnPrint.Location = New System.Drawing.Point(875, 613)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(211, 55)
+        Me.btnPrint.TabIndex = 4
+        Me.btnPrint.Text = "Print"
+        Me.btnPrint.UseVisualStyleBackColor = False
         '
-        'ProjectDatabaseDataSet
+        'btnSave
         '
-        Me.ProjectDatabaseDataSet.DataSetName = "ProjectDatabaseDataSet"
-        Me.ProjectDatabaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        Me.btnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnSave.BackColor = System.Drawing.Color.Gold
+        Me.btnSave.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnSave.Location = New System.Drawing.Point(665, 613)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(211, 55)
+        Me.btnSave.TabIndex = 5
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = False
         '
-        'ScheduleTableBindingSource
+        'MonColumn
         '
-        Me.ScheduleTableBindingSource.DataMember = "ScheduleTable"
-        Me.ScheduleTableBindingSource.DataSource = Me.ProjectDatabaseDataSet
+        Me.MonColumn.HeaderText = ""
+        Me.MonColumn.Name = "MonColumn"
         '
-        'ScheduleTableTableAdapter
+        'MonColumn2
         '
-        Me.ScheduleTableTableAdapter.ClearBeforeFill = True
+        Me.MonColumn2.HeaderText = "Monday"
+        Me.MonColumn2.Name = "MonColumn2"
         '
-        'TimeColumn
+        'MonColumn3
         '
-        DataGridViewCellStyle2.Format = "t"
-        DataGridViewCellStyle2.NullValue = Nothing
-        Me.TimeColumn.DefaultCellStyle = DataGridViewCellStyle2
-        Me.TimeColumn.HeaderText = "Times"
-        Me.TimeColumn.Name = "TimeColumn"
-        Me.TimeColumn.ReadOnly = True
-        Me.TimeColumn.Width = 60
+        Me.MonColumn3.DividerWidth = 2
+        Me.MonColumn3.HeaderText = ""
+        Me.MonColumn3.Name = "MonColumn3"
         '
-        'MondayColumn
+        'TueColumn
         '
-        Me.MondayColumn.HeaderText = "Monday"
-        Me.MondayColumn.Name = "MondayColumn"
-        Me.MondayColumn.ReadOnly = True
-        Me.MondayColumn.Width = 175
+        Me.TueColumn.HeaderText = ""
+        Me.TueColumn.Name = "TueColumn"
         '
-        'TuesdayColumn
+        'TueColumn2
         '
-        Me.TuesdayColumn.HeaderText = "Tuesday"
-        Me.TuesdayColumn.Name = "TuesdayColumn"
-        Me.TuesdayColumn.ReadOnly = True
-        Me.TuesdayColumn.Width = 175
+        Me.TueColumn2.HeaderText = "Tuesday"
+        Me.TueColumn2.Name = "TueColumn2"
         '
-        'WednesdayColumn
+        'TueColumn3
         '
-        Me.WednesdayColumn.HeaderText = "Wednesday"
-        Me.WednesdayColumn.Name = "WednesdayColumn"
-        Me.WednesdayColumn.ReadOnly = True
-        Me.WednesdayColumn.Width = 175
+        Me.TueColumn3.DividerWidth = 2
+        Me.TueColumn3.HeaderText = ""
+        Me.TueColumn3.Name = "TueColumn3"
         '
-        'ThursdayColumn
+        'WedColumn
         '
-        Me.ThursdayColumn.HeaderText = "Thursday"
-        Me.ThursdayColumn.Name = "ThursdayColumn"
-        Me.ThursdayColumn.ReadOnly = True
-        Me.ThursdayColumn.Width = 175
+        Me.WedColumn.HeaderText = ""
+        Me.WedColumn.Name = "WedColumn"
         '
-        'frmSchedual
+        'WedColumn2
+        '
+        Me.WedColumn2.HeaderText = "Wednesday"
+        Me.WedColumn2.Name = "WedColumn2"
+        '
+        'WedColumn3
+        '
+        Me.WedColumn3.DividerWidth = 2
+        Me.WedColumn3.HeaderText = ""
+        Me.WedColumn3.Name = "WedColumn3"
+        '
+        'ThurColumn
+        '
+        Me.ThurColumn.HeaderText = ""
+        Me.ThurColumn.Name = "ThurColumn"
+        '
+        'ThurColumn2
+        '
+        Me.ThurColumn2.HeaderText = "Thursday"
+        Me.ThurColumn2.Name = "ThurColumn2"
+        '
+        'ThurColumn3
+        '
+        Me.ThurColumn3.HeaderText = ""
+        Me.ThurColumn3.Name = "ThurColumn3"
+        '
+        'SchedualForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Maroon
-        Me.ClientSize = New System.Drawing.Size(811, 690)
+        Me.ClientSize = New System.Drawing.Size(1317, 680)
+        Me.Controls.Add(Me.btnSave)
         Me.Controls.Add(Me.btnPrint)
         Me.Controls.Add(Me.btnEmployees)
-        Me.Controls.Add(Me.btnMenu)
-        Me.Controls.Add(Me.ScheduleGrid)
-        Me.Name = "frmSchedual"
-        Me.Text = "Schedule"
-        CType(Me.ScheduleGrid, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.ProjectDatabaseDataSet, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.ScheduleTableBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Controls.Add(Me.btnBack)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "SchedualForm"
+        Me.Text = "Schedule Form"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents ScheduleGrid As DataGridView
-    Friend WithEvents btnPrint As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnBack As Button
     Friend WithEvents btnEmployees As Button
-    Friend WithEvents btnMenu As Button
-    Friend WithEvents ProjectDatabaseDataSet As ProjectDatabaseDataSet
-    Friend WithEvents ScheduleTableBindingSource As BindingSource
-    Friend WithEvents ScheduleTableTableAdapter As ProjectDatabaseDataSetTableAdapters.ScheduleTableTableAdapter
-    Friend WithEvents TimeColumn As DataGridViewTextBoxColumn
-    Friend WithEvents MondayColumn As DataGridViewTextBoxColumn
-    Friend WithEvents TuesdayColumn As DataGridViewTextBoxColumn
-    Friend WithEvents WednesdayColumn As DataGridViewTextBoxColumn
-    Friend WithEvents ThursdayColumn As DataGridViewTextBoxColumn
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents MonColumn As DataGridViewTextBoxColumn
+    Friend WithEvents MonColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents MonColumn3 As DataGridViewTextBoxColumn
+    Friend WithEvents TueColumn As DataGridViewTextBoxColumn
+    Friend WithEvents TueColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents TueColumn3 As DataGridViewTextBoxColumn
+    Friend WithEvents WedColumn As DataGridViewTextBoxColumn
+    Friend WithEvents WedColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents WedColumn3 As DataGridViewTextBoxColumn
+    Friend WithEvents ThurColumn As DataGridViewTextBoxColumn
+    Friend WithEvents ThurColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents ThurColumn3 As DataGridViewTextBoxColumn
 End Class
--- CoffeeShop/CoffeeShop/SchedualForm.vb
+++ CoffeeShop/CoffeeShop/SchedualForm.vb
@@ -1,26 +1,95 @@
-﻿Public Class SchedualForm
-    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnMenu.Click
+﻿Imports System.Drawing.Printing
+Imports System.IO
+
+Public Class SchedualForm
+    Const MANUALSCHEDULE_PATH = "ManualSchedule.dat"
+
+    'OPEN MAIN MENU FORM AND CLOSE THIS ONE?
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        'TODO; Open main menu form.
         Me.Close()
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    'OPEN EMPLOYEE FORM AND CLOSE THIS ONE?
+    Private Sub btnEmployees_Click(sender As Object, e As EventArgs) Handles btnEmployees.Click
+        'TODO; Open employees form.
+    End Sub
+
+    'SAVE ON FORM CLOSE
+    Private Sub SchedualForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        btnSave.PerformClick()
+    End Sub
+
+    'LOAD FUNCTIONALITY; not from database but instead from file
+    Private Sub SchedualForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'ProjectDatabaseDataSet.EmployeeScheduleTable' table. You can move, or remove it, as needed.
-        Me.EmployeeScheduleTableTableAdapter.Fill(Me.ProjectDatabaseDataSet.EmployeeScheduleTable)
-        'For i As Decimal = 7 To 19.5 Step 0.5
-        For i As Decimal = 420 To 1170 Step 30
+        'Me.EmployeeScheduleTableTableAdapter.Fill(Me.ProjectDatabaseDataSet.EmployeeScheduleTable)
+        If File.Exists(MANUALSCHEDULE_PATH) Then
+            Dim fReader As StreamReader = File.OpenText(MANUALSCHEDULE_PATH)
+            For i As Decimal = 420 To 1170 Step 30
+                Dim line As String() = fReader.ReadLine().Split(vbTab)
+                DataGridView1.Rows.Add(line(0), line(1), line(2), line(3), line(4), line(5), line(6), line(7), line(8))
+                DataGridView1.Rows(DataGridView1.Rows.Count - 1).HeaderCell.Value = New DateTime().AddMinutes(i).ToShortTimeString()
+            Next
+            fReader.Close()
+        Else
+            For i As Decimal = 420 To 1170 Step 30
+                DataGridView1.Rows.Add("", "", "", "", "", "", "", "", "", "", "", "")
+                DataGridView1.Rows(DataGridView1.Rows.Count - 1).HeaderCell.Value = New DateTime().AddMinutes(i).ToShortTimeString()
+            Next
+        End If
+    End Sub
 
-            ScheduleGrid.Rows.Add(New DateTime().AddMinutes(i), "m", "t", "w", "th")
+    'SAVE FUNCTIONALITY; not to database but instead to file
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        'get the file as a write object
+        Dim fWriter As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(MANUALSCHEDULE_PATH, False)
+        'get each row in the datagridview and write the strings to file
+        For Each row As DataGridViewRow In DataGridView1.Rows
+            fWriter.WriteLine(String.Join(vbTab, Array.ConvertAll(row.Cells.Cast(Of DataGridViewCell).ToArray(),
+                Function(c) If(c.Value IsNot Nothing, c.Value.ToString(), ""))))
         Next
+        fWriter.Close()
     End Sub
 
-    Private Sub ScheduleGrid_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles ScheduleGrid.CellContentClick
+    'PRINTING FUNCTIONALITY
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        'opens a print dialog that can handle a simple print of the datagrid view
+        Dim printDialog As New PrintDialog()
+        Dim printDoc As New PrintDocument()
+        printDoc.DocumentName = "IHCC Schedule"
+        printDoc.DefaultPageSettings.Landscape = True
+        printDoc.DefaultPageSettings.Color = False
+        AddHandler printDoc.PrintPage, AddressOf OnPrintPage
+        printDialog.Document = printDoc
 
+        If (printDialog.ShowDialog() = DialogResult.OK) Then
+            printDoc.Print()
+        End If
     End Sub
 
-    Private Sub btnMenu_Click(sender As Object, e As EventArgs) Handles btnMenu.Click
-        Dim menu = New frmMenu()
-        Me.Hide()
-        menu.Show()
+    Private Sub OnPrintPage(ByVal sender As Object, ByVal e As System.Drawing.Printing.PrintPageEventArgs)
+        'create a memory bitmap and size to the form
+        Using bmp As Bitmap = New Bitmap(Me.Width, Me.Height)
 
+            'Draws the form/component as a bitmap image
+            DataGridView1.DrawToBitmap(bmp, New Rectangle(0, 0, Me.Width, Me.Height))
+
+            'draw the form image on the printer graphics sized and centered to margins
+            Dim ratio As Single = CSng(bmp.Width / bmp.Height)
+            If ratio > e.MarginBounds.Width / e.MarginBounds.Height Then
+                e.Graphics.DrawImage(bmp,
+                e.MarginBounds.Left,
+                CInt(e.MarginBounds.Top + (e.MarginBounds.Height / 2) - ((e.MarginBounds.Width / ratio) / 2)),
+                e.MarginBounds.Width,
+                CInt(e.MarginBounds.Width / ratio))
+            Else
+                e.Graphics.DrawImage(bmp,
+                CInt(e.MarginBounds.Left + (e.MarginBounds.Width / 2) - (e.MarginBounds.Height * ratio / 2)),
+                e.MarginBounds.Top,
+                CInt(e.MarginBounds.Height * ratio),
+                e.MarginBounds.Height)
+            End If
+        End Using
     End Sub
 End Class
